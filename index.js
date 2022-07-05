e97a8f96325bc7296daf8425e63a2d41c3b110cb
Gucci Fan <gucc@umich.edu>
2022-07-05 18:42:08 -0400

    commit tree 831a732dfb927749d50b191fe90812e088e4c649

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let cardArray = []
+    for(let i=0;i<names.length;i++){
+        let cardMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        cardArray.push(cardMessage)
+    }
+    return cardArray
+}
+function countDown(number){
+    while (number>=0){
+    console.log(number--)
+    }
+    return number
+}
